9cf2dc5b472c544352a4677d9c0144a345284868
daito-kawasaki <daito.wb.b@gmail.com>
2025-01-09 16:10:15 +0900

    geminiapiをUI上で叩けた

---
+++ app/gemini/action.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export async function postAction(prompt: string) {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const result = await model.generateContent(prompt);
+  return result.response.text();
+}
---
+++ app/gemini/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import { postAction } from "./action";
+
+export default function Page() {
+  var [result, setResult] = useState("");
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const res = await postAction(formData.get("prompt") as string);
+      setResult(res);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <div className="p-10">
+      <h1 className="text-white">Gemini</h1>
+
+      <form action={handleSubmit}>
+        <input type="text" name="prompt" className=" text-black w-2/5" />
+        <button type="submit" className=" text-white">
+          送信
+        </button>
+      </form>
+
+      <div className=" text-white">{result}</div>
+    </div>
+  );
+}
